65be368450e025760222de6f92f7a710aaaadf74
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2014-12-26 22:51:54 -0500

    Add sample app. Finished on Jan 5, 2014

---
+++ sample_app/README.md
@@ -0,0 +1,12 @@
+# Ruby on Rails Tutorial: sample application
+
+This is the sample application for
+the [*Ruby on Rails Tutorial*](http://railstutorial.org/)
+by [Michael Hartl](http://michaelhartl.com/).
+
+Project completed in 2 days.
+
+All tests passing:
+
+Finished in 8.12 seconds
+133 examples, 0 failures
---
+++ sample_app/spec/requests/static_pages_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  subject { page }
+
+    describe "Home page" do
+      before { visit root_path }
+
+      it { should have_content('Sample App') }
+      it { should have_title("Ruby on Rails Tutorial Sample App") }
+      it { should_not have_title('| Home') }
+
+      describe "for signed-in users" do
+        let(:user) { FactoryGirl.create(:user) }
+        before do
+          FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+          FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+          sign_in user
+          visit root_path
+        end
+
+        it "should render the user's feed" do
+          user.feed.each do |item|
+            expect(page).to have_selector("li##{item.id}", text: item.content)
+          end
+        end
+
+        describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+      end
+    end
+
+
+    describe "Help page" do
+
+      it "should have the content 'Help'" do
+        visit help_path
+        expect(page).to have_content('Help')
+      end
+
+      it "should have the title 'Help'" do
+        visit help_path
+        expect(page).to have_title("#{base_title} | Help")
+      end
+    end
+
+    describe "About page" do
+
+      it "should have the content 'About Us'" do
+        visit about_path
+        expect(page).to have_content('About Us')
+      end
+
+      it "should have the title 'About Us'" do
+        visit about_path
+        expect(page).to have_title("#{base_title} | About Us")
+      end
+    end
+
+    describe "Contact page" do
+
+      it "should have the content 'Contact'" do
+        visit contact_path
+        expect(page).to have_content('Contact')
+      end
+
+      it "should have the title 'Contact'" do
+        visit contact_path
+        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      end
+    end
+
+end
+
+
+  
